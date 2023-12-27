dc55d308e462b87048f687e46a51f916b698a361
Rishabh Dwivedi <rd14121999@gmail.com>
2023-12-27 16:17:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Video from "./components/video/Video";
+import Feed from "./components/feed/Feed";
+function App() {
+  return (
+    <div className="App">
+      <Video />
+      <Feed />
+    </div>
+  );
+}
+
+export default App;
