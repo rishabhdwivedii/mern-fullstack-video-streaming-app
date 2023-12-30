9afc26d5da47d2356d0b6767998f1e467d35f84f
Rishabh Dwivedi <114381791+rishabhdwivedii@users.noreply.github.com>
2023-12-30 14:52:05 +0530

    Update VideoCard.jsx

--- src/components/videoCard/VideoCard.jsx
+++ src/components/videoCard/VideoCard.jsx
@@ -4,7 +4,7 @@ import "./videoCard.css";
 
 const VideoCard = ({ video }) => {
   return (
-    <Link to="/videoPage/1" className="video">
+        <Link to={`/videoPage/${video.id}`} className="video">
       <img className="thumbnail" src={video.thumbnail} alt="Thumbnail" />
       <div className="title">{video.title}</div>
     </Link>
