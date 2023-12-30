07ad5502984c77cbb53ba4d1217f3081bc5a2f62
Rishabh Dwivedi <114381791+rishabhdwivedii@users.noreply.github.com>
2023-12-30 14:55:12 +0530

    Update VideoPage.jsx

--- src/pages/videoPage/VideoPage.jsx
+++ src/pages/videoPage/VideoPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { VIDEOS } from "../../videos";
 import SideFeed from "../../components/sideFeed/SideFeed";
 import "./videoPage.css";
 
 export default function VideoPage() {
+  const { id } = useParams();
+  const selectedVideo = VIDEOS.find((video) => video.id.toString() === id);
+  const videoTitle = selectedVideo ? selectedVideo.title : "Video Not Found";
   return (
     <div className="video-page">
       <div className="page">
@@ -14,7 +17,7 @@ export default function VideoPage() {
             controls
             src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
           />
-          <div className="title">Just another video</div>
+          <div className="title">{videoTitle}</div>
         </div>
         <div className="feedd">
           {VIDEOS.map((video) => (
